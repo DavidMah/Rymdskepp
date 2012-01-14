8bc0b48fe60e3d38833298b6dde6772dbd2ffbde
David Mah <mahhaha@gmail.com>
2012-01-14 15:29:48 -0800

    Messages can now be passed pretty cleanly

--- server/game_server.rb
+++ server/game_server.rb
@@ -1,17 +1,61 @@
 require 'web_socket'
+require 'json'
+
+MESSAGE_BOILER = {"action" => "game_server"}
 
 
 class GameServer
   def initialize(socket)
-    socket.send('{"action":"set_game_server"}')
+    @socket = socket
+    @socket.send('{"action":"game_server", "command":"set_game_server"}')
+    @outbox = []
+
+    @users = {}
   end
 
   def request_messages
-    puts "requesting..."
+    puts "requesting messages..."
+    @socket.send('{"action":"game_server", "command":"request_messages"}')
+
+    puts "TEST #{@users.inspect}"
+  end
+
+  def handle_data(messages)
+    begin
+      messages = JSON.parse(messages)
+      puts "!!!#{messages}"
+      puts "handling #{messages.size} messages"
+      messages.each do |msg|
+        puts "=>#{msg}"
+        puts "==>#{msg['message']}"
+        puts "==>#{msg['message']['action']}"
+        action = msg['message']['action']
+        send("handle_#{action}", msg['message'], msg['socket'])
+      end
+    rescue => ex
+      puts "\033[31m#{ex}\n#{ex.backtrace[0..3].join("\n")}\033[0m"
+    end
   end
 
   def send_messages
-    puts "sending..."
+    puts "sending...#{@outbox.size} messages"
+    container = MESSAGE_BOILER
+    container = container.merge({"command"  => "send_messages"})
+    container = container.merge({"messages" => @outbox})
+    @socket.send(container.to_json)
+    @outbox = []
+  end
+
+  # Responses
+  def handle_new_socket(message, socket_id)
+    @users[socket_id] = {}
+  end
+
+  def handle_new_player(message, socket_id)
+    name = message['name']
+    code = message['code']
+    @users[socket_id]['name'] = name
+    @users[socket_id]['code'] = code
   end
 end
 
@@ -20,7 +64,8 @@ server = GameServer.new(socket)
 
 i = 0
 loop do
-  server.request_messages
-  server.send_messages
-  sleep 1
+  server.request_messages()
+  server.handle_data(socket.receive())
+  server.send_messages()
+  sleep(0.3)
 end
--- server/socket_server.rb
+++ server/socket_server.rb
@@ -4,23 +4,61 @@ require 'em-websocket'
 
 class SocketServer
   def initialize
-    @sockets = []
+    @sockets = {}
     @game_server = nil
+    @top_id = 0
+
+    @outbox = []
+  end
+
+  def add_socket(socket)
+    @sockets[socket] = @top_id
+    @sockets[@top_id] = socket
+    add_to_outbox({"action" => "new_socket"}, socket)
+    @top_id += 1
   end
 
   def handle(message, socket)
     begin
       message = JSON.parse(message)
-      self.send("handle_#{message['action']}", socket, message)
+      if message['action'] == 'game_server'
+        send("handle_gs_#{message['command']}", socket, message)
+      elsif 
+        add_to_outbox(message, socket)
+      end
     rescue => ex
-      puts ex
+      puts "\033[31m#{ex}\n#{ex.backtrace[0..3].join("\n")}\033[0m"
     end
   end
 
   # Once set, the game server can't change
-  def handle_set_game_server(socket, message)
+  def handle_gs_set_game_server(socket, message)
     puts "setting game server" if not @game_server
     @game_server ||= socket
+  end
+
+  def handle_gs_request_messages(socket, message)
+    @game_server.send @outbox.to_json
+    @outbox = []
+  end
+
+  # {"messages" => [
+  #    {"id" => 1,
+  #     "message" => "--------------"
+  #    }
+  # ...]}
+  def handle_gs_send_messages(socket, message)
+    items = message['messages']
+    puts "sending out #{items.size} messages to clients..."
+    items.each do |item|
+      outbound_id = item['id']
+      outbound_socket = @sockets[outbound_id]
+      outbound_socket.send item['message']
+    end
+  end
+
+  def add_to_outbox(message, socket)
+    @outbox << {"socket" => @sockets[socket], "message" => message}
   end
 end
 
@@ -30,7 +68,7 @@ i = 0
 EventMachine.run {
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 9001) do |socket|
     socket.onopen {
-      puts "Opened socket"
+      socket_server.add_socket(socket)
     }
 
     socket.onmessage do |msg|
